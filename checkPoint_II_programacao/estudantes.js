90e57a904fa607c898ac890005488ae3cb4799c8
Henrique-Schneider <domeneider@gmail.com>
2022-06-29 19:10:45 -0300

    atualizando a funcao faltas

--- checkPoint_II_programacao/estudantes.js
+++ checkPoint_II_programacao/estudantes.js
@@ -13,10 +13,11 @@ function aluno(nome = " ", qtfaltas, notas = []) {
     return this.media;
   })();
 
-  this.faltas = function (faltas) {
+  this.faltas = function () {
     let numeroFaltas = this.qtfaltas;
     let soma = 0;
     soma = numeroFaltas + 1;
+    this.qtfaltas = soma;
     return soma;
   };
 }
@@ -31,11 +32,9 @@ let aluno5 = new aluno("Analice Andrade", 9, [70, 70, 70]);
 
 //----------- somar faltas -----------//
 
-console.log("O numero de faltas do aluno" +" "+`${aluno5.nome}`+" "+'é:' + aluno5.faltas(1));
-
+//console.log("O numero de faltas do aluno" +" "+`${aluno5.nome}`+" "+'é:' + aluno5.faltas(1));
 
 /*Passo 3 - crie o objeto literal curso que tem como atributos: nome do curso (string), nota de aprovação (number), faltas máximas (number) e uma lista de estudantes (um array composto pelos alunos criados no passo 2).*/
-
 
 const curso = {
   nomeDoCurso: "Certified Tech Developer",
@@ -55,14 +54,18 @@ const curso = {
       let faltas = alunoCurso.qtfaltas;
       let aprovado =
         (media >= this.notaAprovacao && faltas < this.faltasMaximas) ||
-        (faltas == this.faltasMaximas && media >= media10) ?
-        true :
-        false;
+        (faltas == this.faltasMaximas && media >= media10)
+          ? 'Aluno aprovado!'
+          : 'Aluno reprovado!';
+          
       if (alunoCurso.nome == aluno) {
         console.log(
           "Aluno encontrado!!!",
           "\n",
           alunoCurso.nome,
+          "\n",
+          "Faltas:",
+          faltas,
           "\n",
           "media:",
           media,
@@ -78,7 +81,6 @@ const curso = {
   },
 
   resultadoDosAlunos() {
-
     let resultado = [];
 
     for (let i = 0; i < this.listaAlunos.length; i++) {
@@ -88,38 +90,37 @@ const curso = {
       let faltas = alunoCurso.qtfaltas;
       let aprovado =
         (media >= this.notaAprovacao && faltas < this.faltasMaximas) ||
-        (faltas == this.faltasMaximas && media >= media10) ?
-        true :
-        false;
+        (faltas == this.faltasMaximas && media >= media10)
+          ? true
+          : false;
 
       resultado.push(aprovado);
     }
     console.log(resultado);
     return resultado;
-  }
+  },
 };
-
 
 /*Passo 4 -Crie o método que permite adicionar alunos à lista do curso, ou seja, quando chamamos nosso método em nosso objeto curso, deverá adicionar um aluno a mais na propriedade lista de estudantes do objeto curso.*/
 
 //----------- cadastrar aluno -----------//
-curso.cadastrarAluno("Shaianne Pereira", 0, [30,70,70])
+curso.cadastrarAluno("Shaianne Pereira", 0, [30, 70, 70]);
 
 //----------- lista dos alunos -----------//
-console.log('Curso:',curso.nomeDoCurso.toUpperCase())
+console.log("Curso:", curso.nomeDoCurso.toUpperCase());
 console.log(curso.listaAlunos)
-
 
 
 
 /*Passo 5 - Crie um método para o objeto curso que receba um aluno (como parâmetro) e retorne true se ele aprovou no curso ou false em caso de reprovação. Para ser aprovado, o aluno tem que ter uma média igual ou acima da nota de aprovação  e ter menos faltas que faltas máximas. Se tiver a mesma quantidade, tem que estar 10% acima da nota de aprovação.*/
 
-//----------- status do aluno-----------//
+//----------- status do aluno-----------//O status do aluno muda de 'aprovado' para 'reprovado' se chamar a 'Função faltas' no console.log.
 
-//curso.statusAluno('Henrique Schneider')
+//console.log("O numero de faltas do aluno" + " " + `${aluno5.nome}` + " " + "é:" +  aluno5.faltas());
+
+//curso.statusAluno("Analice Andrade");
 
 /*Passo 6 - Crie um método para o objeto curso que percorra a lista de estudantes e retorne um array de booleanos com os resultados se os alunos aprovaram ou não. */
 
 //----------- array de resultados -----------//
-curso.resultadoDosAlunos()
-
+curso.resultadoDosAlunos();
